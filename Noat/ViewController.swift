c4e5cca2e401aea935623e77c6e007a5b1dc5c54
rolpe <ronlipkin14@gmail.com>
2018-07-21 20:44:25 -0400

    Initial Commit

---
+++ Noat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noat
+//
+//  Created by Ron Lipkin on 7/21/18.
+//  Copyright © 2018 Ron Lipkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
